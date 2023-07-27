c1a474cc576558552a451a8a9bc517f962d3e248
Subhajit Ghosh <sg2418@it.jgec.ac.in>
2023-07-27 22:07:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+
+
+const connect=async()=>{
+try {
+  await mongoose.connect(process.env.MONGO);
+  console.log("connented to db");
+} catch (error) {
+   throw error;
+ }
+}
+
+
+app.use(cookieParser())
+app.use(express.json());
+
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
+  
+
+
+
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+  });
+
+app.listen(8800,()=>{
+    connect();
+    console.log("connected to backend");
+})
